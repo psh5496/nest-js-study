ef638c6e84e245193ffc3b7cf1eb061801cdb7a2
psh549633 <psh549633@gmail.com>
2022-07-27 17:45:18 +0900

    :card_file_box: :: entity를 실제 DB에 연결

--- src/movies/entities/movies.entity.ts
+++ src/movies/entities/movies.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Movie {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   title: string;
+
+  @Column()
+  director: string;
+
+  @Column()
   year: number;
-  genres: string[];
 }
