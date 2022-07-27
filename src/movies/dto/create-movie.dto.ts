8a89334b06e1ca3dc0f695747ce13875193d711a
psh549633 <psh549633@gmail.com>
2022-07-27 17:46:42 +0900

    :hammer: :: dto 변경

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -4,10 +4,9 @@ export class CreateMovieDto {
   @IsString()
   readonly title: string;
 
+  @IsString()
+  readonly director: string;
+
   @IsNumber()
   readonly year: number;
-
-  @IsOptional()
-  @IsString({ each: true })
-  readonly genres: string[];
 }
