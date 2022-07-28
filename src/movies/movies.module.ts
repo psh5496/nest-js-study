f53b8ae824e3db824ea54f572b38c9a1884a1112
psh549633 <psh549633@gmail.com>
2022-07-28 11:32:53 +0900

    :hammer: :: movies 모듈에서 DB에 접근이 가능하도록 설정

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Movie } from './entities/movies.entity';
 import { MoviesController } from './movies.controller';
 import { MoviesService } from './movies.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Movie])],
+  exports: [TypeOrmModule],
   controllers: [MoviesController],
   providers: [MoviesService],
 })
